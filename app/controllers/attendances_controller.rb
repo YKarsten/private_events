6bf37879219f8c3d3c8cddb33719280daa2d4dd0
YKarsten <yannik.karsten@web.de>
2023-05-13 14:14:56 +0200

    setup attendances controller #create & #destroy

---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,21 @@
+class AttendancesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    user = current_user
+    @events = Event.all
+    event = Event.find(params[:attended_event_id])
+    attendance = event.attendances.new(user: user)
+    if attendance.save
+      redirect_to user_path(current_user.id), notice: 'You have successfully joined the event!'
+    else
+      flash.now[:errors] = attendance.errors.full_messages
+      render 'events/index', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Attendance.find(params[:id]).destroy
+    redirect_to events_path, notice: 'Sorry to see you go!'
+  end
+end
