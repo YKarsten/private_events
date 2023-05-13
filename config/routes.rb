64496243fc1a2ce2fd5ebcb1e5acd193565051db
YKarsten <yannik.karsten@web.de>
2023-05-13 14:04:56 +0200

    nest attendances within events

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
-  get 'event/index'
+  resources :events, only: %i[index show new create edit update] do
+    resources :attendances, only: %i[create destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
