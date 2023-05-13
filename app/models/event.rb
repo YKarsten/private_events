d918e91b2d103e33edb01356532bd4e111e4da3f
YKarsten <yannik.karsten@web.de>
2023-05-13 14:02:11 +0200

    Setup attendances model and associations with event and user

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :host, class_name: 'User'
+  has_many :attendances
+  has_many :attendees, thorugh: :attendances, source: :user
 
   validates :title, presence: true
   validates :date, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :hosted_Events, foreign_key: :host_id, class_name: 'Event'
+  has_many :attendances
+  has_many :attended_events, through: :attendances, source: :event
 
   validates :username, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
