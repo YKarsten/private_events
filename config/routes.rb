b4b6bdbc9ff0de7bdd4cab7f176468e1de2a16ef
YKarsten <yannik.karsten@web.de>
2023-05-13 13:07:18 +0200

    Setup Users controller and #show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   get 'event/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
