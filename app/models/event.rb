51fbf1ad46eff45f3adc68ad348b18471b5f0cce
YKarsten <yannik.karsten@web.de>
2023-05-13 12:40:41 +0200

    Set User-Event associations and validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ApplicationRecord
+  belongs_to :host, class_name: 'User'
+
+  validates :title, presence: true
+  validates :date, presence: true
+  validates :location, presence: true
 end
