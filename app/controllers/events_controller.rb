9d60e62f7935b8c44017022b6443bfe34a2dcd5a
YKarsten <yannik.karsten@web.de>
2023-05-13 13:17:29 +0200

    set #new & #create action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,29 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @events = Event.all
+  end
+
+  def new
+    @user = current_user
+    @event = Event.new
+  end
+
+  def create
+    @user = current_user
+    @event = @user.hosted_events.build(event_params)
+    if @event.save
+      redirect_to root_url
+    else
+      flash.now[:errors] = @event.errors.full_messages
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date, :location)
   end
 end
